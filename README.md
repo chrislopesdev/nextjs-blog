d5bc31844d9a007e8e4e7f7a444df7f583eeb2e0
Chris Lopes <chris.lopes@gmail.com>
2022-04-27 15:04:21 +0000

    API Routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+In nextjs-blog directory in terminal type; `npm run dev`
--- components/date.js
+++ components/date.js
@@ -4,3 +4,12 @@ export default function Date({ dateString }) {
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
+
+// Express server on port 3000
+// $ npm run dev
+// $ open http://localhost:3000
+// Return the current time in the format:
+// "January 1, 2020"
+// "January 1, 2020, 5:30 PM"
+// "January 1, 2020, 5:30 PM EST"
+// "January 1, 2020, 5:30 PM Eastern Standard Time"
